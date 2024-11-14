5115f286ae904263c4fefa70ba69bb5a3148fb3a
baljinder <baljindermaan15@gmail.com>
2024-11-14 15:18:17 -0500

    first commit

---
+++ Guess.cs
@@ -0,0 +1,16 @@
+namespace GuessingGame
+{
+    public class Guess
+    {
+        public int UserGuess { get; }
+        public DateTime GuessTime { get; }
+
+        public Guess(int userGuess)
+        {
+            UserGuess = userGuess;
+            GuessTime = DateTime.Now;
+        }
+
+
+    }
+}
